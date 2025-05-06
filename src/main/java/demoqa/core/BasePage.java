3359f91ed1f2b97828892d4a9851f5655e6d52f7
anyat <anyatarara93@gmail.com>
2025-05-06 20:43:38 +0200

    L32

--- src/main/java/demoqa/core/BasePage.java
+++ src/main/java/demoqa/core/BasePage.java
@@ -1,6 +1,7 @@
 package demoqa.core;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
@@ -9,15 +10,24 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 public class BasePage {
     public WebDriver driver;
     public WebDriverWait wait;
+    public JavascriptExecutor js;
 
     public BasePage(WebDriver driver, WebDriverWait wait) {
         this.driver = driver;
         this.wait = wait;
+        this.js = (JavascriptExecutor) driver;
         PageFactory.initElements(driver, this);
     }
 
     public void click(WebElement element) {
         element.click();
+    }
+
+    public void click(WebElement element, int x, int y) {
+        //element.click();
+        //js.executeScript("window.scrollBy("+x+","+y+")");
+        js.executeScript("window.scrollBy({},{})", x, y);
+        click(element);
     }
 
     public void type(WebElement element, String text) {
@@ -26,5 +36,18 @@ public class BasePage {
             element.clear();
             element.sendKeys(text);
         }
+    }
+
+    public void type(WebElement element, String text, int y) {
+        if (text != null) {
+            click(element, 0, y);
+            element.clear();
+            element.sendKeys(text);
+        }
+    }
+
+    public void hideAds() {
+        js.executeScript("document.getElementById('adplus-anchor').style.display='none';");
+        js.executeScript("document.querySelector('footer').style.display='none';");
     }
 }
--- src/main/java/demoqa/pages/ProfilePage.java
+++ src/main/java/demoqa/pages/ProfilePage.java
@@ -1,4 +1,28 @@
 package demoqa.pages;
 
-public class ProfilePage {
+import demoqa.core.BasePage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+
+public class ProfilePage extends BasePage {
+    public ProfilePage(WebDriver driver, WebDriverWait wait) {
+        super(driver, wait);
+    }
+
+    @FindBy(id = "userName-value")
+    WebElement userNameValue;
+
+
+    public ProfilePage verifyUserName(String text) {
+//        assert userNameValue.getText().contains(text) : "Username does not contains the expected text.";
+        String expectedText = text;
+        String actualText = userNameValue.getText();
+        String errorMessage = "\nExpected text: " + expectedText + ",\nActual text: " + actualText;
+        System.out.println("Expected text: " + expectedText + ",\nActual text: " + actualText);
+        Assert.assertTrue(actualText.contains(expectedText), errorMessage);
+        return this;
+    }
 }
